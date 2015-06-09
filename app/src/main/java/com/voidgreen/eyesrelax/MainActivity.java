6fc5c2da6df94197b28c4a9bd613b771b8a11016
y.shlapak <y.shlapak@samsung.com>
2015-06-09 23:18:16 +0300

    Refactoring
    Signed-off-by: y.shlapak <y.shlapak@samsung.com>

--- app/src/main/java/com/voidgreen/eyesrelax/MainActivity.java
+++ app/src/main/java/com/voidgreen/eyesrelax/MainActivity.java
@@ -1,9 +1,11 @@
-package com.eyesrelax.yshlapak.eyesrelax;
+package com.voidgreen.eyesrelax;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.eyesrelax.yshlapak.eyesrelax.R;
 
 
 public class MainActivity extends ActionBarActivity {
