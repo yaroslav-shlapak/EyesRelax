9b4f20ccca24283d39ae064d2ddbd5171a014a63
y.shlapak <y.shlapak@samsung.com>
2015-06-25 19:34:10 +0300

    Preferences were implemented
    Signed-off-by: y.shlapak <y.shlapak@samsung.com>

--- app/src/main/java/com/voidgreen/eyesrelax/MainActivity.java
+++ app/src/main/java/com/voidgreen/eyesrelax/MainActivity.java
@@ -1,6 +1,8 @@
 package com.voidgreen.eyesrelax;
 
 import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
@@ -56,6 +58,8 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+
+
 /*        mProgress = (ProgressBar) findViewById(R.id.yourId);
 
         // Start lengthy operation in a background thread
@@ -85,26 +89,29 @@ public class MainActivity extends ActionBarActivity {
         return mProgressStatus >= 100 ? 0 : mProgressStatus++;
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Context context = getApplicationContext();
+                Intent intent = new Intent(this, EyesRelaxSettingsActivity.class);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                this.startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
